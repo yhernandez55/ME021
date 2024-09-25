36dba846902d088f2dc553a531b78c0047922926
Yanelly Hernandez <100453560+yhernandez55@users.noreply.github.com>
2024-09-25 14:04:01 -0700

    Add files via upload

---
+++ Plotting_functions.m
@@ -0,0 +1,16 @@
+clear all
+close all 
+clc
+syms x
+x= [0:0.01:2];
+f= (x-1)./(x.^32-1);
+figure;
+hold on
+plot(x,subs(f))
+axis([0 2 -0.2 1.2])
+xlabel('x vals')
+ylabel('f(x)')
+title('f(x)= (x-1)/(x^32-1)');
+xline(1,'--','x=1')
+yline(1/32,'--','f(1)=1/32','linewidth', 0.5);
+grid on
